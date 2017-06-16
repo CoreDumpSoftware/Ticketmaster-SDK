f336ffcd50c5ee691c7394ce20cf13c30aa50f62
Serhii_Voznyi <serhiivoznyi@gmail.com>
2017-06-16 20:59:08 +0300

    Updated AssemblyInfo File.

--- src/TM-SDK/TM.Discovery/Properties/AssemblyInfo.cs
+++ src/TM-SDK/TM.Discovery/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TM.Discovery")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ticketmaster-SDK")]
+[assembly: AssemblyDescription("This SDK supports Discovery API v2.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TM.Discovery")]
-[assembly: AssemblyCopyright("Copyright © Serhii Voznyi 2016")]
+[assembly: AssemblyCompany("QQ.Development.Team")]
+[assembly: AssemblyProduct("Ticketmaster-SDK: TM.Discovery")]
+[assembly: AssemblyCopyright("Copyright © Serhii Voznyi 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
